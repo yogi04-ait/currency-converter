cae16fa49ea4d3eee70f8e8589c8b5e4fdd45783
yogesh aithani <aithaniyogesh007@gmail.com>
2024-04-05 23:12:06 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CurrencyConvertor from "./components/currency-convertor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-main  flex flex-col items-center justify-center">
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
     </div>
   );
 }
